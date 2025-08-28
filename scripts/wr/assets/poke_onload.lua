b377b6307f4d303c23aa4c4cee931933b85640c0
WasabiRaptor <ketchupraptor@gmail.com>
2025-08-28 13:58:43 -0400

    generate images for intro cinematic and quests patch

--- scripts/wr/assets/poke_onload.lua
+++ scripts/wr/assets/poke_onload.lua
@@ -9,111 +9,145 @@ local charcreation_patch = {}
 local universe_server_patch = {}
 local ai_patch = {}
 local player_patch = {}
+local quests_patch = {}
 for _, species in ipairs(races) do
-    local speciesConfig = assets.json("/species/" .. species .. ".species")
-    table.insert(charcreation_patch, { op = "add", path = "/speciesOrdering/-", value = species })
-    universe_server_patch = sb.jsonMerge(universe_server_patch, {
-        speciesShips = {
-            [species] = speciesConfig.ships or {
-                "/ships/novakid/novakidt0.structure",
-                "/ships/novakid/novakidt1.structure",
-                "/ships/novakid/novakidt2.structure",
-                "/ships/novakid/novakidt3.structure",
-                "/ships/novakid/novakidt4.structure",
-                "/ships/novakid/novakidt5.structure",
-                "/ships/novakid/novakidt6.structure",
-                "/ships/novakid/novakidt7.structure",
-                "/ships/novakid/novakidt8.structure"
-            }
-        }
-    })
-    ai_patch = sb.jsonMerge(ai_patch, {
-		species = {
-			[species] = speciesConfig.ai or {
-				aiFrames = "NovakidAI.png",
-				portraitFrames = "portraits/novakidportrait.png",
-				staticFrames = "staticGlitch.png"
-			},
+	local speciesConfig = assets.json("/species/" .. species .. ".species")
+	table.insert(charcreation_patch, { op = "add", path = "/speciesOrdering/-", value = species })
+	universe_server_patch = sb.jsonMerge(universe_server_patch, {
+		speciesShips = {
+			[species] = speciesConfig.ships or {
+				"/ships/novakid/novakidt0.structure",
+				"/ships/novakid/novakidt1.structure",
+				"/ships/novakid/novakidt2.structure",
+				"/ships/novakid/novakidt3.structure",
+				"/ships/novakid/novakidt4.structure",
+				"/ships/novakid/novakidt5.structure",
+				"/ships/novakid/novakidt6.structure",
+				"/ships/novakid/novakidt7.structure",
+				"/ships/novakid/novakidt8.structure"
+			}
 		}
-    })
+	})
+	if not assets.exists("/objects/protectorate/objects/protectorateship/protectorateship" .. species .. ".png") then
+		assets.add("/objects/protectorate/objects/protectorateship/protectorateship" .. species .. ".png",
+			assets.image("/objects/protectorate/objects/protectorateship/protectorateshipnovakid.png"))
+	end
+	if not assets.exists("/cinematics/story/smallship/" .. species .. "ship.png") then
+		assets.add("/cinematics/story/smallship/" .. species .. "ship.png",
+			assets.image("/cinematics/story/smallship/novakidship.png"))
+	end
+	if not assets.exists("/cinematics/story/smallship/" .. species .. "ship4.png") then
+		assets.add("/cinematics/story/smallship/" .. species .. "ship4.png",
+			assets.image("/cinematics/story/smallship/novakidship4.png"))
+	end
+	if not assets.exists("/cinematics/story/smallship/" .. species .. "ship3.png") then
+		assets.add("/cinematics/story/smallship/" .. species .. "ship3.png",
+			assets.image("/cinematics/story/smallship/novakidship3.png"))
+	end
+	if not assets.exists("/cinematics/story/smallship/" .. species .. "ship2.png") then
+		assets.add("/cinematics/story/smallship/" .. species .. "ship2.png",
+			assets.image("/cinematics/story/smallship/novakidship2.png"))
+	end
 
-    player_patch = sb.jsonMerge(player_patch, {
-        defaultCodexes = {
-            [species] = speciesConfig.defaultCodexes or { "protectorate1", "protectorate2" }
-        }
-    })
 
-    if not assets.exists("/cinematics/teleport/deploy_" .. species .. ".cinematic") then
-        assets.add("/cinematics/teleport/deploy_" .. species .. ".cinematic",
-            assets.bytes(speciesConfig.deployCinematic or "/cinematics/teleport/deploy_wr/template.cinematic"):gsub(
-                "%<species%>", species):gsub("%<effectDirectives%>", speciesConfig.effectDirectives))
-    end
-    if not assets.exists("/cinematics/teleport/teleport_" .. species .. ".cinematic") then
-        assets.add("/cinematics/teleport/teleport_" .. species .. ".cinematic",
-            assets.bytes(speciesConfig.teleportCinematic or "/cinematics/teleport/teleport_wr/template.cinematic"):gsub(
-                "%<species%>", species):gsub("%<effectDirectives%>", speciesConfig.effectDirectives))
-    end
-    if not assets.exists("/cinematics/respawn/" .. species .. "/respawncasual.cinematic") then
-        assets.add("/cinematics/respawn/" .. species .. "/respawncasual.cinematic",
-            assets.bytes(speciesConfig.teleportCinematic or "/cinematics/respawn/wr/template/respawncasual.cinematic")
-            :gsub("%<species%>", species):gsub("%<effectDirectives%>", speciesConfig.effectDirectives))
-    end
-    if not assets.exists("/cinematics/respawn/" .. species .. "/respawnsurvival.cinematic") then
-        assets.add("/cinematics/respawn/" .. species .. "/respawnsurvival.cinematic",
-            assets.bytes(speciesConfig.teleportCinematic or "/cinematics/respawn/wr/template/respawnsurvival.cinematic")
-            :gsub("%<species%>", species):gsub("%<effectDirectives%>", speciesConfig.effectDirectives))
-    end
+	local ai = speciesConfig.ai or {
+		aiFrames = "NovakidAI.png",
+		portraitFrames = "portraits/novakidportrait.png",
+		staticFrames = "staticGlitch.png"
+	}
+	ai_patch = sb.jsonMerge(ai_patch, {
+		species = {
+			[species] = ai
+		}
+	})
+	if not assets.exists("/ai/portraits/" .. species .. "portrait.png") then
+		assets.add("/ai/portraits/" .. species .. "portrait.png",
+			assets.image("/ai/"..ai.portraitFrames))
+	end
 
-    if not assets.exists("/interface/ai/suits/" .. species .. "-female.png") then
-        assets.add("/interface/ai/suits/" .. species .. "-female.png",
-            assets.image("/interface/ai/suits/wr/template-female.png"))
-    end
-    if not assets.exists("/interface/ai/suits/" .. species .. "-male.png") then
-        assets.add("/interface/ai/suits/" .. species .. "-male.png",
-            assets.image("/interface/ai/suits/wr/template-male.png"))
-    end
+	player_patch = sb.jsonMerge(player_patch, {
+		defaultCodexes = {
+			[species] = speciesConfig.defaultCodexes or { "protectorate1", "protectorate2" }
+		}
+	})
+	quests_patch = sb.jsonMerge(quests_patch, {
+		initialquests = {
+			[species] = speciesConfig.initialquests or { "protectorate" }
+		}
+	})
 
-    if not assets.exists("/interface/scripted/techupgrade/suits/" .. species .. "-female.png") then
-        assets.add("/interface/scripted/techupgrade/suits/" .. species .. "-female.png",
-            assets.image("/interface/scripted/techupgrade/suits/wr/template-female.png"))
-    end
-    if not assets.exists("/interface/scripted/techupgrade/suits/" .. species .. "-female-body.png") then
-        assets.add("/interface/scripted/techupgrade/suits/" .. species .. "-female-body.png",
-            assets.image("/interface/scripted/techupgrade/suits/wr/template-female-body.png"))
-    end
-    if not assets.exists("/interface/scripted/techupgrade/suits/" .. species .. "-female-head.png") then
-        assets.add("/interface/scripted/techupgrade/suits/" .. species .. "-female-head.png",
-            assets.image("/interface/scripted/techupgrade/suits/wr/template-female-head.png"))
-    end
-    if not assets.exists("/interface/scripted/techupgrade/suits/" .. species .. "-female-legs.png") then
-        assets.add("/interface/scripted/techupgrade/suits/" .. species .. "-female-legs.png",
-            assets.image("/interface/scripted/techupgrade/suits/wr/template-female-legs.png"))
-    end
+	if not assets.exists("/cinematics/teleport/deploy_" .. species .. ".cinematic") then
+		assets.add("/cinematics/teleport/deploy_" .. species .. ".cinematic",
+			assets.bytes(speciesConfig.deployCinematic or "/cinematics/teleport/deploy_wr/template.cinematic"):gsub(
+				"%<species%>", species):gsub("%<effectDirectives%>", speciesConfig.effectDirectives))
+	end
+	if not assets.exists("/cinematics/teleport/teleport_" .. species .. ".cinematic") then
+		assets.add("/cinematics/teleport/teleport_" .. species .. ".cinematic",
+			assets.bytes(speciesConfig.teleportCinematic or "/cinematics/teleport/teleport_wr/template.cinematic"):gsub(
+				"%<species%>", species):gsub("%<effectDirectives%>", speciesConfig.effectDirectives))
+	end
+	if not assets.exists("/cinematics/respawn/" .. species .. "/respawncasual.cinematic") then
+		assets.add("/cinematics/respawn/" .. species .. "/respawncasual.cinematic",
+			assets.bytes(speciesConfig.teleportCinematic or "/cinematics/respawn/wr/template/respawncasual.cinematic")
+			:gsub("%<species%>", species):gsub("%<effectDirectives%>", speciesConfig.effectDirectives))
+	end
+	if not assets.exists("/cinematics/respawn/" .. species .. "/respawnsurvival.cinematic") then
+		assets.add("/cinematics/respawn/" .. species .. "/respawnsurvival.cinematic",
+			assets.bytes(speciesConfig.teleportCinematic or "/cinematics/respawn/wr/template/respawnsurvival.cinematic")
+			:gsub("%<species%>", species):gsub("%<effectDirectives%>", speciesConfig.effectDirectives))
+	end
 
-    if not assets.exists("/interface/scripted/techupgrade/suits/" .. species .. "-male.png") then
-        assets.add("/interface/scripted/techupgrade/suits/" .. species .. "-male.png",
-            assets.image("/interface/scripted/techupgrade/suits/wr/template-male.png"))
-    end
-    if not assets.exists("/interface/scripted/techupgrade/suits/" .. species .. "-male-body.png") then
-        assets.add("/interface/scripted/techupgrade/suits/" .. species .. "-male-body.png",
-            assets.image("/interface/scripted/techupgrade/suits/wr/template-male-body.png"))
-    end
-    if not assets.exists("/interface/scripted/techupgrade/suits/" .. species .. "-male-head.png") then
-        assets.add("/interface/scripted/techupgrade/suits/" .. species .. "-male-head.png",
-            assets.image("/interface/scripted/techupgrade/suits/wr/template-male-head.png"))
-    end
-    if not assets.exists("/interface/scripted/techupgrade/suits/" .. species .. "-male-legs.png") then
-        assets.add("/interface/scripted/techupgrade/suits/" .. species .. "-male-legs.png",
-            assets.image("/interface/scripted/techupgrade/suits/wr/template-male-legs.png"))
-    end
+	if not assets.exists("/interface/ai/suits/" .. species .. "-female.png") then
+		assets.add("/interface/ai/suits/" .. species .. "-female.png",
+			assets.image("/interface/ai/suits/wr/template-female.png"))
+	end
+	if not assets.exists("/interface/ai/suits/" .. species .. "-male.png") then
+		assets.add("/interface/ai/suits/" .. species .. "-male.png",
+			assets.image("/interface/ai/suits/wr/template-male.png"))
+	end
 
-    if not assets.exists("/ai/portraits/" .. species .. "questportrait.png") then
-        assets.add("/ai/portraits/" .. species .. "questportrait.png",
-            assets.image("/ai/portraits/novakidquestportrait.png"))
-    end
+	if not assets.exists("/interface/scripted/techupgrade/suits/" .. species .. "-female.png") then
+		assets.add("/interface/scripted/techupgrade/suits/" .. species .. "-female.png",
+			assets.image("/interface/scripted/techupgrade/suits/wr/template-female.png"))
+	end
+	if not assets.exists("/interface/scripted/techupgrade/suits/" .. species .. "-female-body.png") then
+		assets.add("/interface/scripted/techupgrade/suits/" .. species .. "-female-body.png",
+			assets.image("/interface/scripted/techupgrade/suits/wr/template-female-body.png"))
+	end
+	if not assets.exists("/interface/scripted/techupgrade/suits/" .. species .. "-female-head.png") then
+		assets.add("/interface/scripted/techupgrade/suits/" .. species .. "-female-head.png",
+			assets.image("/interface/scripted/techupgrade/suits/wr/template-female-head.png"))
+	end
+	if not assets.exists("/interface/scripted/techupgrade/suits/" .. species .. "-female-legs.png") then
+		assets.add("/interface/scripted/techupgrade/suits/" .. species .. "-female-legs.png",
+			assets.image("/interface/scripted/techupgrade/suits/wr/template-female-legs.png"))
+	end
+
+	if not assets.exists("/interface/scripted/techupgrade/suits/" .. species .. "-male.png") then
+		assets.add("/interface/scripted/techupgrade/suits/" .. species .. "-male.png",
+			assets.image("/interface/scripted/techupgrade/suits/wr/template-male.png"))
+	end
+	if not assets.exists("/interface/scripted/techupgrade/suits/" .. species .. "-male-body.png") then
+		assets.add("/interface/scripted/techupgrade/suits/" .. species .. "-male-body.png",
+			assets.image("/interface/scripted/techupgrade/suits/wr/template-male-body.png"))
+	end
+	if not assets.exists("/interface/scripted/techupgrade/suits/" .. species .. "-male-head.png") then
+		assets.add("/interface/scripted/techupgrade/suits/" .. species .. "-male-head.png",
+			assets.image("/interface/scripted/techupgrade/suits/wr/template-male-head.png"))
+	end
+	if not assets.exists("/interface/scripted/techupgrade/suits/" .. species .. "-male-legs.png") then
+		assets.add("/interface/scripted/techupgrade/suits/" .. species .. "-male-legs.png",
+			assets.image("/interface/scripted/techupgrade/suits/wr/template-male-legs.png"))
+	end
+
+	if not assets.exists("/ai/portraits/" .. species .. "questportrait.png") then
+		assets.add("/ai/portraits/" .. species .. "questportrait.png",
+			assets.image("/ai/portraits/novakidquestportrait.png"))
+	end
 end
 
 assets.add("/interface/windowconfig/charcreation.config.patch", sb.printJson(charcreation_patch))
 assets.add("/universe_server.config.patch", sb.printJson(universe_server_patch))
 assets.add("/ai/ai.config.patch", sb.printJson(ai_patch))
 assets.add("/player.config.patch", sb.printJson(player_patch))
+assets.add("/quests/quests.config.patch", sb.printJson(quests_patch))
