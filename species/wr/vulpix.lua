a588f01c4a21b98698616f81c93980c1faeb2321
WasabiRaptor <ketchupraptor@gmail.com>
2025-10-15 19:38:03 -0400

    vulpix colors

--- species/wr/vulpix.lua
+++ species/wr/vulpix.lua
@@ -1,28 +1,35 @@
 -- these are just the names of the variables internally and how they're used on the creation screen, do whatever you want with them here!
 -- but probably put name species and gender as the ones supplied, thats probably a good idea
 -- I've supplied the bare minimum as a template here, do with it what you will
-local function wrap(index, array)
-	if index < #array then return index end
-	return math.fmod(index, #array)
+local function wrap(index, length, secondary, secondaryLength)
+	if not index then
+		index = math.floor(wrap(secondary, secondaryLength * length) / secondaryLength)
+	end
+	if index < length then return index end
+	return math.fmod(index, length)
 end
-function create(name, species, genderIndex, furColor, hairColor, pawsColor, hairStyle, bellyColor, shirtColor, eyeColor, pantsColor, personality, ...)
+function create(name, species, genderIndex, furColor, hairColor, pawsColor, hairStyle, bellyColor, markingsColor, eyeColor, pantsColor, personality, tailMarkingsColor, ...)
 	-- these values are zero indexed!
 
 	local speciesConfig = root.speciesConfig(species)
 	local humanoidConfig = sb.jsonMerge(root.assetJson(speciesConfig.humanoidConfig or "/humanoid.config"), speciesConfig.humanoidOverrides or {})
 
+	tailMarkingsColor = wrap(tailMarkingsColor, #speciesConfig.tailMarkingsColor, markingsColor, #speciesConfig.markingsColor)
 
-	genderIndex = wrap(genderIndex, speciesConfig.genders)
-	furColor = wrap(furColor, speciesConfig.furColor)
-	pawsColor = wrap(pawsColor, speciesConfig.pawsColor)
-	bellyColor = wrap(bellyColor, speciesConfig.bellyColor)
-	hairColor = wrap(hairColor, speciesConfig.hairColor)
-	eyeColor = wrap(eyeColor, speciesConfig.eyeColor)
-	hairStyle = wrap(hairStyle, speciesConfig.hairStyle)
-	personality = wrap(personality, humanoidConfig.personalities)
+	genderIndex = wrap(genderIndex, #speciesConfig.genders)
+	furColor = wrap(furColor, #speciesConfig.furColor)
+	pawsColor = wrap(pawsColor, #speciesConfig.pawsColor)
+	bellyColor = wrap(bellyColor, #speciesConfig.bellyColor)
+	hairColor = wrap(hairColor, #speciesConfig.hairColor)
+	eyeColor = wrap(eyeColor, #speciesConfig.eyeColor)
+	hairStyle = wrap(hairStyle, #speciesConfig.hairStyle)
+	markingsColor = wrap(markingsColor, #speciesConfig.markingsColor)
+	personality = wrap(personality, #humanoidConfig.personalities)
 
 	local directives = ""
 
+	directives = directives..(speciesConfig.tailMarkingsColor[tailMarkingsColor+1])
+	directives = directives..(speciesConfig.markingsColor[markingsColor+1])
 	directives = directives..(speciesConfig.furColor[furColor+1])
 	directives = directives..(speciesConfig.pawsColor[pawsColor+1])
 	directives = directives..(speciesConfig.bellyColor[bellyColor+1])
@@ -53,7 +60,7 @@ function create(name, species, genderIndex, furColor, hairColor, pawsColor, hair
 		color = {51, 117, 237, 255},
 	}
 	local parameters = {
-		choices = { genderIndex, furColor, hairColor, pawsColor, hairStyle, bellyColor, shirtColor, eyeColor, pantsColor, personality, ... },
+		choices = { genderIndex, furColor, hairColor, pawsColor, hairStyle, bellyColor, markingsColor, eyeColor, pantsColor, personality, ... },
 		--this you can do a lot with, see the humanoid build script
 	}
 	local armor = {
